1e9772eea40da39dbf2ab1088ce097814980d2ed
crangelsmith <camila.rangel.smith@gmail.com>
2020-12-07 11:54:53 +0000

    refactoring the counterfactual simulation function (#10)

--- src/simulate_counterfactuals.py
+++ src/simulate_counterfactuals.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+from src.process_data import get_important_dates, get_number_of_cases
 
-def simulate_country_counterfactuals(country, restrictions_shifts, df_cases, best_knots_points, df_summaries):
+
+def simulate_country_counterfactuals(country, counterfactual_shift, df_cases, best_knots_points, df_summaries):
     """
     Function that simulates the evolution of Covi19 cases for a country, given a shift in the time
     the restrictions where applied.
@@ -10,7 +12,7 @@ def simulate_country_counterfactuals(country, restrictions_shifts, df_cases, bes
     ----------
     country: str
         Country to be simulated
-    restrictions_shifts: tuple
+    counterfactual_shift: tuple
         Number of days earlier to implement FIRST RESTRICTION and LOCKDOWN
     df_cases: dataframe
         Historical case data.
@@ -27,66 +29,57 @@ def simulate_country_counterfactuals(country, restrictions_shifts, df_cases, bes
 
     # Filter datasets by country
     knots_best_country = best_knots_points[best_knots_points["Country"] == country]  # best knots
-    data_eur_country = df_cases[df_cases['Country'] == country]
-    summary_eur_country = df_summaries[df_summaries["Country"] == country]
 
     # Record important dates
+    dates = get_important_dates(country, df_summaries)
     # date for which cumulative cases first exceeded this percent (Date_pop_pct)
-    initial_date = pd.to_datetime(summary_eur_country.Date_pop_pct, format="%Y-%m-%d")
+    initial_date = dates['initial_date']
 
     # lastdate to include data
     # Date_max, Date_restrictions_eased + 28, or Date_lockdown_eased + 28, whichever comes first
-    maximum_date = pd.to_datetime(summary_eur_country.Date_T, format="%Y-%m-%d")
-    # date_first_restriction = summary_eur_country.Date_first_restriction
-    # date_lockdown = summary_eur_country.Date_lockdown
+    maximum_date = dates['maximum_date']
 
     # convert to dates to datetime
     knots_best_country['Knot_date_1'] = pd.to_datetime(knots_best_country['Knot_date_1'], format="%Y-%m-%d")
     knots_best_country['Knot_date_2'] = pd.to_datetime(knots_best_country['Knot_date_2'], format="%Y-%m-%d")
-    data_eur_country['Date'] = pd.to_datetime(data_eur_country['Date'])
 
     # Calculate maximum number of days earlier we can estimate FIRST RESTRICTION
-    # (minimum value of knot_date_1 greater than or equal to initial_date)
+    # (minimum value of knot_date_1 greater than or equal to date)
     max_days_counterfactual_first_restriction = min(
-        knots_best_country.Knot_date_1 - initial_date.repeat(knots_best_country.shape[0]).values).days
-    max_days_counterfactual_lockdown = min(knots_best_country.Knot_date_2 - (initial_date + pd.DateOffset(1)).repeat(
-        knots_best_country.shape[0]).values).days
+        knots_best_country.Knot_date_1 - pd.Series(initial_date).repeat(knots_best_country.shape[0]).values).days
+    max_days_counterfactual_lockdown = min(
+        knots_best_country.Knot_date_2 - pd.Series(initial_date + pd.DateOffset(1)).repeat(
+            knots_best_country.shape[0]).values).days
 
     # Determine all possible combinations of counterfactual days
     # (filter so that minimum value of knot_date_2 is always greater than knot_date_1)
-    x = pd.DataFrame(np.array([(x, y) for x in range(max_days_counterfactual_first_restriction + 1) for y in
+    possibles = pd.DataFrame(np.array([(x, y) for x in range(max_days_counterfactual_first_restriction + 1) for y in
                                range(max_days_counterfactual_lockdown + 1)]),
                      columns=['Poss_days_counterfactual_first_restriction', 'Poss_days_counterfactual_lockdown'])
-    possible_days_counterfactual = x[
-        (x['Poss_days_counterfactual_lockdown'] - x['Poss_days_counterfactual_first_restriction']) <= (
-                    max_days_counterfactual_lockdown - max_days_counterfactual_first_restriction - 1)]
+    possible_days_counterfactual = possibles[
+        (possibles['Poss_days_counterfactual_lockdown'] - possibles['Poss_days_counterfactual_first_restriction']) <= (
+                max_days_counterfactual_lockdown - max_days_counterfactual_first_restriction - 1)]
 
     match = possible_days_counterfactual[
-        (possible_days_counterfactual['Poss_days_counterfactual_first_restriction'] == restrictions_shifts[0]) \
-        & (possible_days_counterfactual['Poss_days_counterfactual_lockdown'] == restrictions_shifts[1])]
+        (possible_days_counterfactual['Poss_days_counterfactual_first_restriction'] == counterfactual_shift[0]) \
+        & (possible_days_counterfactual['Poss_days_counterfactual_lockdown'] == counterfactual_shift[1])]
 
     if (match.shape[0] == 0):
         raise RuntimeError("Stop - cannot estimate counterfactual")
 
     # get initial number of cases for the first day of the simulation
-    initial_case_number = data_eur_country[data_eur_country['Date'] == (initial_date.values[0] - pd.Timedelta(days=1))][
-        'Daily_cases_MA7'].values
-
-    # get initial cumulative number of cases for the first day of the simulation
-    initial_cumulative_case_number = \
-    data_eur_country[data_eur_country['Date'] == (initial_date.values[0] - pd.Timedelta(days=1))][
-        'Cumulative_cases_end_MA7'].values
+    initial_case_number, initial_cumulative_case_number = get_number_of_cases(country, df_cases, initial_date)
 
     # run simulation
-    daily_cases_sim = simulate_counterfactuals(knots_best_country, initial_date, maximum_date, initial_case_number,  \
-                                               restrictions_shifts)
+    daily_cases_sim = simulate_counterfactuals(knots_best_country, initial_date, maximum_date, initial_case_number, \
+                                               counterfactual_shift)
 
     # get mean value of time series.
     daily_cases_sim_mean = daily_cases_sim.mean(axis=0)
 
     daily_cases_sim_cum = daily_cases_sim.copy()
     daily_cases_sim_cum.loc[:,
-    (initial_date.values[0] - pd.Timedelta(days=1)).strftime("%m-%d-%Y")] = initial_cumulative_case_number.repeat( \
+    (initial_date - pd.Timedelta(days=1)).strftime("%m-%d-%Y")] = initial_cumulative_case_number.repeat( \
         daily_cases_sim_cum.shape[0])
     daily_cases_sim_cum = daily_cases_sim_cum.reindex(sorted(daily_cases_sim_cum.columns), axis=1)
 
@@ -97,7 +90,7 @@ def simulate_country_counterfactuals(country, restrictions_shifts, df_cases, bes
     return daily_cases_sim_mean, daily_cases_sim_cum_sum_mean
 
 
-def simulate_counterfactuals(knots_best, initial_date, maximum_date, initial_case_number, restrictions_shifts):
+def simulate_counterfactuals(knots_best, initial_date, maximum_date, initial_case_number, counterfactual_shift):
     """
 
     Function that runs the simulation of the epidemic history given an initial case number, growth parameters and
@@ -114,7 +107,7 @@ def simulate_counterfactuals(knots_best, initial_date, maximum_date, initial_cas
         Maximum date to run the simulation
     initial_case_number:
         Initial case numbers observed at the initial date
-    restrictions_shifts: tuple
+    counterfactual_shift: tuple
         Number of days earlier to implement FIRST RESTRICTION and LOCKDOWN
 
     Returns
@@ -124,14 +117,14 @@ def simulate_counterfactuals(knots_best, initial_date, maximum_date, initial_cas
 
     """
 
-    n_days_counterfactual_first_restriction = restrictions_shifts[0]
-    n_days_counterfactual_lockdown = restrictions_shifts[1]
+    n_days_counterfactual_first_restriction = counterfactual_shift[0]
+    n_days_counterfactual_lockdown = counterfactual_shift[1]
 
     knots_best['Knot_date_1'] = knots_best['Knot_date_1'] - pd.Timedelta(days=n_days_counterfactual_first_restriction)
     knots_best['Knot_date_2'] = knots_best['Knot_date_2'] - pd.Timedelta(days=n_days_counterfactual_lockdown)
 
     # Set dates to simulate
-    dates = pd.date_range(start=initial_date.values[0], end=maximum_date.values[0], freq='D').tolist()
+    dates = pd.date_range(start=initial_date, end=maximum_date, freq='D').tolist()
 
     daily_cases_sim = pd.DataFrame()
 
@@ -154,11 +147,11 @@ def simulate_counterfactuals(knots_best, initial_date, maximum_date, initial_cas
         n_runs_i = knots_best_country_counterfactual_i['N_unequal']
 
         daily_cases_sim_i = pd.DataFrame(index=[i for i in range(n_runs_i)],
-                                         columns=pd.date_range(start=initial_date.values[0] - pd.Timedelta(days=1),
-                                                               end=maximum_date.values[0],
+                                         columns=pd.date_range(start=initial_date - pd.Timedelta(days=1),
+                                                               end=maximum_date,
                                                                freq='D').strftime("%m-%d-%Y").tolist())
 
-        daily_cases_sim_i.iloc[:, 0] = initial_case_number.repeat(n_runs_i)
+        daily_cases_sim_i.iloc[:, 0] = [initial_case_number for _ in range(n_runs_i)]
 
         for date in dates:
             inc_tminus1 = daily_cases_sim_i[(date - pd.Timedelta(days=1)).strftime("%m-%d-%Y")]
@@ -189,5 +182,3 @@ def simulate_counterfactuals(knots_best, initial_date, maximum_date, initial_cas
         daily_cases_sim = pd.concat([daily_cases_sim, daily_cases_sim_i])
 
     return daily_cases_sim
-
-
