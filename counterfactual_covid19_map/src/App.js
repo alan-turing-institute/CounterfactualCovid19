3070d7bc4d036f6e987fbe0ce78a11768029317c
crangelsmith <camila.rangel.smith@gmail.com>
2021-01-07 12:03:03 +0000

    uploading initial react-leaflet code

---
+++ counterfactual_covid19_map/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Covid19 from "./components/Covid19";
+
+function App() {
+  return (
+    <div>
+      <Covid19 />
+    </div>
+  );
+}
+
+export default App;
