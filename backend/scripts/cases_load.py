72e57b197c58c3fb5e69e0556e162450fc175914
crangelsmith <camila.rangel.smith@gmail.com>
2021-01-18 12:49:19 +0000

    updating script that loads case data to database

--- backend/scripts/cases_load.py
+++ backend/scripts/cases_load.py
@@ -4,59 +4,32 @@ import pandas as pd
 import pycountry
 from cases.models import Cases
 
+
 def run():
 
-    url_cases = 'https://raw.githubusercontent.com/alan-turing-institute/CounterfactualCovid19-inputs/develop/Data/Formatted/Cases_deaths_data_europe.csv'
-    df_cases = pd.read_csv(url_cases)
+    # source data processed by @KFArnold which serves at source to the counterfactual simulation and the total datasets
+    url_cases = "https://raw.githubusercontent.com/alan-turing-institute/CounterfactualCovid19-inputs/develop/Data/Formatted/Cases_deaths_data_europe.csv"
 
-    countries = df_cases.Country.unique()
-    date = '2020-06-23'
+    #dummy date for end of the first wave in Europe
+    end_date = "2020-06-23"
 
+    # Load all cases then filter by date
+    df_cases = pd.read_csv(url_cases, parse_dates=["Date"])
+    df_cases_end_date = df_cases[df_cases["Date"] == end_date]
+
+    # Delete all existing Cases data and regenerate the table
     Cases.objects.all().delete()
-
-    for country in countries:
-
-        ts_data_country_daily, ts_data_country_cum = get_natural_history_data(country, df_cases)
-
-        total_cases = ts_data_country_daily[ts_data_country_daily.index < date].sum()
-
+    for entry in df_cases_end_date.itertuples():
         try:
-            code_county = pycountry.countries.get(name=country)
-
-            m = Cases(country=country, iso_code=code_county.alpha_3, date=date, cumulative_cases=total_cases)
-
+            code_county = pycountry.countries.get(name=entry.Country)
+            if not code_county:
+                code_county = pycountry.countries.search_fuzzy(entry.Country)[0]
+            m = Cases(
+                country=code_county.name,
+                iso_code=code_county.alpha_3,
+                date=end_date,
+                cumulative_cases=entry.Cumulative_cases_end,
+            )
             m.save()
         except AttributeError:
             continue
-
-
-def get_natural_history_data(country, df_cases):
-    """
-
-    Parameters
-    ----------
-    country: str
-        Country for data to be fetched
-    df_cases: dataframe
-        Historical case data.
-
-    Returns
-    -------
-     Two time series with the data from daily and cumulative cases.
-
-
-    """
-
-    # select country
-    data_country_daily = df_cases[df_cases['Country'] == country]
-    data_country_cum = df_cases[df_cases['Country'] == country]
-
-    # reindex with date
-    data_country_daily.set_index('Date', inplace=True)
-    data_country_cum.set_index('Date', inplace=True)
-
-    # get time series
-    data_country_daily = data_country_daily['Daily_cases']
-    data_country_cum = data_country_cum['Cumulative_cases_end']
-
-    return data_country_daily, data_country_cum
