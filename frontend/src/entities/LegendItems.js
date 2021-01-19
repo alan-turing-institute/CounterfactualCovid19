177d86ad4cdbfe62c3c98c7a6f67face81983c59
crangelsmith <camila.rangel.smith@gmail.com>
2021-01-19 17:27:00 +0000

    adapt leyend to total cases per million #26

--- frontend/src/entities/LegendItems.js
+++ frontend/src/entities/LegendItems.js
@@ -2,37 +2,37 @@ import LegendItem from "./LegendItem";
 
 var legendItems = [
   new LegendItem(
-    "1,000,000 +",
+    "10000 +",
     "#741f1f",
     // "#8b0000",
-    (cases) => cases >= 1_000_000,
+    (cases) => cases >= 10000,
     "white"
   ),
 
   new LegendItem(
-    "500,000 - 999,999",
+    "5000 - 9999",
     // "#741f1f",
     "#9c2929",
-    (cases) => cases >= 500_000 && cases < 1_000_000,
+    (cases) => cases >= 5000 && cases < 10000,
     "White"
   ),
 
   new LegendItem(
-    "200,000 - 499,999",
+    "1000 - 4999",
     "#c57f7f",
-    (cases) => cases >= 200_000 && cases < 500_000
+    (cases) => cases >= 1000 && cases < 5000
   ),
 
   new LegendItem(
-    "50,000 - 199,999",
+    "500 - 999",
     "#d8aaaa",
-    (cases) => cases >= 50_000 && cases < 200_000
+    (cases) => cases >= 500 && cases < 1000
   ),
 
   new LegendItem(
-    "0 - 49,999",
+    "0 - 499",
     "#ebd4d4",
-    (cases) => cases > 0 && cases < 50_000
+    (cases) => cases > 0 && cases < 500
   ),
 
   new LegendItem("No Data", "#808080", (cases) => true),
