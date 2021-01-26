64632bbba5f557569a0dbfb0c6244f86fd3b775a
James Robinson <james.em.robinson@gmail.com>
2021-01-26 11:40:38 +0000

    Moved implementation details into model - following 'fat model/thin view' pattern

--- backend/cases/models/ephemeral.py
+++ backend/cases/models/ephemeral.py
@@ -1,9 +1,40 @@
+from .concrete import TimeSeriesCases
+import pandas as pd
+
+
 class DailyCounterfactualCases:
     def __init__(self, iso_code, date, cases, cumulative_cases):
         self.iso_code = iso_code
         self.date = date
         self.cases = cases
         self.cumulative_cases = cumulative_cases
+
+    @staticmethod
+    def simulate_counterfactual_records(iso_codes=[]):
+        """List of dicts containing counterfactual simulations for one or more countries"""
+        # Load data from database
+        df_data = pd.DataFrame(list(TimeSeriesCases.objects.all().values()))
+        df_data.rename(
+            columns={"daily_cases": "cases", "daily_cum_cases": "cumulative_cases"},
+            inplace=True,
+        )
+        # Use all countries if none are provided
+        if not iso_codes:
+            iso_codes = df_data.iso_code.unique()
+        # Simulate each requested country
+        df_counterfactuals = [
+            DailyCounterfactualCases.simulate_single_country(
+                df_data[df_data["iso_code"] == iso_code]
+            )
+            for iso_code in iso_codes
+        ]
+        # Convert to records, flatten/combine and return
+        return sum([df.to_dict("records") for df in df_counterfactuals], [])
+
+    @staticmethod
+    def simulate_single_country(df_country_data):
+        """Counterfactual simulation for a single country"""
+        return df_country_data
 
     def __str__(self):
         return (
--- backend/cases/views.py
+++ backend/cases/views.py
@@ -6,41 +6,32 @@ from .serializers import (
 from .models import TotalCases, TimeSeriesCases, DailyCounterfactualCases
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
-import pandas as pd
 
 
-# Total number of cases
 class TotalCasesView(viewsets.ModelViewSet):
+    """Total number of cases"""
+
     serializer_class = TotalCasesSerializer
     queryset = TotalCases.objects.all()
     http_method_names = ["get", "head", "list", "options"]
 
 
-# Daily and cumulative cases on a date
 class TimeSeriesCasesView(viewsets.ModelViewSet):
+    """Daily and cumulative cases on a date"""
+
     serializer_class = TimeSeriesCasesSerializer
     queryset = TimeSeriesCases.objects.all()
     http_method_names = ["get", "head", "list", "options"]
 
 
 class DailyCounterfactualCasesViewSet(viewsets.ViewSet):
-    # Required for the Browsable API renderer to have a nice form.
+    """Counterfactual daily and cumulative cases on a date"""
+
     serializer_class = DailyCounterfactualCasesSerializer
 
     def list(self, request):
-        counterfactual_records = []
-        df_data = pd.DataFrame(list(TimeSeriesCases.objects.all().values()))
-        df_data.rename(
-            columns={"daily_cases": "cases", "daily_cum_cases": "cumulative_cases"},
-            inplace=True,
-        )
-        for iso_code in df_data.iso_code.unique():
-            df_country = df_data[
-                df_data["iso_code"] == iso_code
-            ]  # data for a single country
-            # Counterfactual simulation is called here
-            counterfactual_records += df_country.to_dict("records")
-        serializer = DailyCounterfactualCasesSerializer(
-            instance=counterfactual_records, many=True
-        )
+        """Response to a GET/LIST request"""
+        print("request", type(request), request)
+        records = DailyCounterfactualCases.simulate_counterfactual_records()
+        serializer = DailyCounterfactualCasesSerializer(instance=records, many=True)
         return Response(serializer.data)
