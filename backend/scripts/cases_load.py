17ded52873df04aca83bfc3af8c96fb821c29f24
James Robinson <james.em.robinson@gmail.com>
2021-01-27 18:03:00 +0000

    :zap: Speed up loading of case data by pre-computing country data and storing in a lookup table

--- backend/scripts/cases_load.py
+++ backend/scripts/cases_load.py
@@ -8,6 +8,21 @@ from countries.models import Country
 from django.core.exceptions import ObjectDoesNotExist
 
 
+def get_country_code(country_name):
+    country_code = pycountry.countries.get(name=country_name)
+    if not country_code:
+        country_code = pycountry.countries.search_fuzzy(country_name)[0]
+    return country_code.alpha_3
+
+
+def get_country_model(iso_code):
+    try:
+        return Country.objects.get(iso_code=iso_code)
+    except ObjectDoesNotExist:
+        print(f"Could not find a matching country for {iso_code}")
+    return None
+
+
 def run():
     print("Starting to load cases data...")
     start = monotonic()
@@ -20,23 +35,29 @@ def run():
 
     # Delete all existing CasesRecord data and regenerate the table
     CasesRecord.objects.all().delete()
+
+    # Add an ISO code column lookup table
+    code_lookup = {
+        country: get_country_code(country) for country in df_cases["Country"].unique()
+    }
+    df_cases["iso_code"] = df_cases.apply(lambda row: code_lookup[row.Country], axis=1)
+
+    # Create a lookup table from ISO code to Country model
+    country_lookup = {
+        iso_code: get_country_model(iso_code)
+        for iso_code in df_cases["iso_code"].unique()
+    }
+
     for entry in df_cases.itertuples():
         try:
-            code_country = pycountry.countries.get(name=entry.Country)
-            if not code_country:
-                code_country = pycountry.countries.search_fuzzy(entry.Country)[0]
-
-            try:
-                country = Country.objects.get(iso_code=code_country.alpha_3)
-            except ObjectDoesNotExist:
-                print(f"Could not find a matching country for {code_country.alpha_3}")
-
-            m = CasesRecord(
-                country=country,
-                date=entry.Date,
-                cases=entry.Daily_cases_MA7,
-            )
-            m.save()
+            country = country_lookup[entry.iso_code]
+            if country:
+                m = CasesRecord(
+                    country=country,
+                    date=entry.Date,
+                    cases=entry.Daily_cases_MA7,
+                )
+                m.save()
 
         except AttributeError:
             continue
