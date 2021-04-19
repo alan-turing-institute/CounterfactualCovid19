7a8e4bc745127571187932807ddcd5f526164ee7
crangelsmith <camila.rangel.smith@gmail.com>
2021-04-19 14:54:30 +0100

    frontend access to country restriction data

--- backend/dates/views.py
+++ backend/dates/views.py
@@ -23,6 +23,15 @@ class ModelDateRangeView(viewsets.ModelViewSet):
     queryset = ModelDateRange.objects.all()  # pylint: disable=no-member
     http_method_names = ["get", "list"]
 
+    def get_queryset(self):
+        """Construct a default queryset with filters"""
+        queryset = ModelDateRange.objects.all()  # pylint: disable=no-member
+        # Apply filter on ISO code
+        iso_code = self.request.query_params.get("iso_code", None)
+        if iso_code:
+            queryset = queryset.filter(country__iso_code=iso_code)
+        return queryset
+
 
 class PossibleDateSetView(viewsets.ModelViewSet):
     """View for PossibleDateSet"""
