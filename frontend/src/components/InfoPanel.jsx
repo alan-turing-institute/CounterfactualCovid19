053c80288d498a587c64ce52d3be4f18dbe331cf
crangelsmith <camila.rangel.smith@gmail.com>
2021-04-21 17:42:05 +0100

    refactoring infopanel to have important dates in the class state

--- frontend/src/components/InfoPanel.jsx
+++ frontend/src/components/InfoPanel.jsx
@@ -5,10 +5,69 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Histogram from "./Histogram";
-import MyDatesPicker from "./MyDatesPicker";
+//import MyDatesPicker from "./MyDatesPicker";
+import LoadRestrictionsDatesTask from "../tasks/LoadRestrictionsDatesTask.js";
+import DatePicker from "react-date-picker";
 
 export default class InfoPanel extends React.Component {
+
+ constructor(props) {
+    super(props);
+
+  this.state = {
+    first_restrictions_date: null,
+    lockdown_date: null,
+    initial_date: null,
+    maximum_date: null,
+  };
+
+
+
+  }
+
+
+   async loadRestrictionData() {
+    // Retrieve Restriction data
+    const task = new LoadRestrictionsDatesTask();
+    let [restrictionsDates] = await Promise.all([
+      task.getCountryRestrictionDates(this.props.isoCode),
+    ]);
+
+    if (restrictionsDates.length != 0 & this.props.isoCode!=null) {
+
+      // Set the component state with the restriction data
+      this.setState({
+        first_restrictions_date: restrictionsDates[0].first_restrictions_date,
+      });
+      this.setState({ lockdown_date: restrictionsDates[0].lockdown_date });
+      this.setState({ initial_date: restrictionsDates[0].initial_date });
+      this.setState({ maximum_date: restrictionsDates[0].maximum_date });
+    }
+  }
+
+  async componentDidMount() {
+    await this.loadRestrictionData();
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (this.props.isoCode !== prevProps.isoCode) {
+      await this.loadRestrictionData();
+    }
+  }
+
+  onChange = (first_restrictions_date) => this.setState({ first_restrictions_date });
+  onChangeTwo = (lockdown_date) => this.setState({ lockdown_date });
+
+
+
+
+
   render() {
+
+    console.log(" This state infopanel")
+    console.log(this.state.first_restrictions_date)
+    console.log(this.state.lockdown_date)
+
     return (
       <div>
         {!this.props.isoCode ? null : (
@@ -27,7 +86,7 @@ export default class InfoPanel extends React.Component {
                       <Card.Title>{`${this.props.countryName}`}</Card.Title>
                       <Card.Text>
                         The first wave for {`${this.props.countryName}`}{" "}
-                        happened between X and X date.
+                        happened between {`${this.state.initial_date}`} and {`${this.state.maximum_date}`} date.
                       </Card.Text>
                     </Card.Body>
                   </Card>
@@ -54,7 +113,19 @@ export default class InfoPanel extends React.Component {
               </Col>
               <Col xs={6} md={6} lg={6}>
                 <Row xs={1} md={1} lg={1}>
-                  <MyDatesPicker />
+                <Row
+                style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            }}>
+                <Col>
+            <DatePicker onChange={this.onChange} value={this.state.first_restrictions_date} />
+            </Col>
+            <Col>
+            <DatePicker onChange={this.onChangeTwo} value={this.state.lockdown_date} />
+                </Col>
+                </Row>
                 </Row>
                 <Row xs={1} md={1} lg={1}>
                   <Histogram
