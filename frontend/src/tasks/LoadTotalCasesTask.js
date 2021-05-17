35feb20443269498a44f627a2e4b5bdf3e6c371b
crangelsmith <camila.rangel.smith@gmail.com>
2021-05-17 15:19:18 +0100

    adding function to retreive integrated conterfactual cases

--- frontend/src/tasks/LoadTotalCasesTask.js
+++ frontend/src/tasks/LoadTotalCasesTask.js
@@ -25,6 +25,66 @@ class LoadTotalCasesTask {
     }
   };
 
+  getIntegratedCasesCountryData = async (iso_code,end_date) => {
+    try {
+      console.log(`http://localhost:8000/api/cases/real/integrated/?iso_code=${iso_code}&end_date=${end_date}`)
+      const res = await axios.get(
+        `http://localhost:8000/api/cases/real/integrated/?iso_code=${iso_code}&end_date=${end_date}`,
+        {}
+      );
+      return res.data[0];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+   getIntegratedCounterfactualCountryData  = async (
+    iso_code,
+    start_date,
+    end_date,
+    first_restriction_date,
+    lockdown_date
+  ) => {
+    try {
+      // in case there is both restriction and lockdown dates
+      if ((lockdown_date != null) & (first_restriction_date != null)) {
+        const res_counterfactual_dates = await axios.get(
+          `http://localhost:8000/api/cases/counterfactual/integrated/?iso_code=${iso_code}&start_date=${start_date}&end_date=${end_date}&first_restriction_date=${first_restriction_date}&lockdown_date=${lockdown_date}`,
+          {}
+        );
+        const size = (res_counterfactual_dates.data).length
+        // return the last date on the model
+        return res_counterfactual_dates.data.last();
+      } else {
+        // some cases there is only first_restriction_date
+        if (first_restriction_date != null) {
+          const res_counterfactual = await axios.get(
+            `http://localhost:8000/api/cases/counterfactual/integrated/?iso_code=${iso_code}&start_date=${start_date}&end_date=${end_date}&first_restriction_date=${first_restriction_date}`,
+            {}
+          );
+          // return the last date on the model
+          return res_counterfactual.data.last();
+        } else {
+          const res_counterfactual_default = await axios.get(
+            `http://localhost:8000/api/cases/counterfactual/integrated/?iso_code=${iso_code}&start_date=${start_date}&end_date=${end_date}`,
+            {}
+          );
+
+          const size = (res_counterfactual_default.data).length
+         // return the last date on the model
+          return res_counterfactual_default.data.last();
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+
+
+
   #processCovidData = (countryGeoms, integratedCasesData) => {
     for (let i = 0; i < countryGeoms.length; i++) {
       // Find matching country
