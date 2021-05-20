767118dcae830d03f709cf43e048842c093b05ea
James Robinson <james.em.robinson@gmail.com>
2021-05-20 18:09:14 +0100

    :bug: Do not attempt to parse invalid dates

--- frontend/src/components/Utils.js
+++ frontend/src/components/Utils.js
@@ -1,4 +1,7 @@
 export default function convert(inputDate) {
+  if (!inputDate) {
+    return inputDate;
+  }
   var date = inputDate,
     mnth = ("0" + (date.getMonth() + 1)).slice(-2),
     day = ("0" + date.getDate()).slice(-2);
