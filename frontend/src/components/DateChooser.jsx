c6d359973acc45f1ac3b631d1954d58a69841f8e
James Robinson <james.em.robinson@gmail.com>
2021-06-01 18:32:14 +0200

    :bug: Revert to previous ISO date constructor as toISOString() uses timezone information that breaks in non-UTC locales

--- frontend/src/components/DateChooser.jsx
+++ frontend/src/components/DateChooser.jsx
@@ -12,7 +12,10 @@ const defaultProps = {};
 const DateChooser = (props) => {
   function stringFromDate(inputDate) {
     try {
-      return inputDate.toISOString().split("T")[0];
+      const year = inputDate.getFullYear();
+      const month = `0${inputDate.getMonth() + 1}`.slice(-2)
+      const day = `0${inputDate.getDate()}`.slice(-2)
+      return `${year}-${month}-${day}`;
     } catch (error) {
       console.log(error);
       return inputDate;
