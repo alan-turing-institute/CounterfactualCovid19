e7c4b5ea74172aa42789644a4b198694a0a1965c
James Robinson <james.em.robinson@gmail.com>
2021-06-08 19:36:38 +0200

    :recycle: Simplified LoadDailyCasesTask functions

--- frontend/src/tasks/LoadDailyCasesTask.js
+++ frontend/src/tasks/LoadDailyCasesTask.js
@@ -1,22 +1,7 @@
 import axios from "axios";
 
 class LoadDailyCasesTask {
-  // function to get real cases for a period of time,
-  #getDailyCovidCases = async (datatype, iso_code, start_date, end_date) => {
-    try {
-      const target = `http://localhost:8000/api/cases/${datatype}/daily/normalised/?iso_code=${iso_code}&start_date=${start_date}&end_date=${end_date}`;
-      console.debug(`Backend ${target}`);
-      const response = await axios.get(target, {});
-      return response.data;
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  };
-
-  // function to get counterfactual cases for given restriction dates for a period of time
-  #getDailyCounterfactualCovidCases = async (
-    datatype,
+  getCounterfactualCovidCases = async (
     iso_code,
     start_date,
     end_date,
@@ -24,7 +9,7 @@ class LoadDailyCasesTask {
     lockdown_date
   ) => {
     try {
-      var target = `http://localhost:8000/api/cases/${datatype}/daily/normalised/?iso_code=${iso_code}&start_date=${start_date}&end_date=${end_date}`;
+      var target = `http://localhost:8000/api/cases/counterfactual/daily/normalised/?iso_code=${iso_code}&start_date=${start_date}&end_date=${end_date}`;
       if (first_restriction_date != null) {
         target = `${target}&first_restriction_date=${first_restriction_date}`;
       }
@@ -40,32 +25,16 @@ class LoadDailyCasesTask {
     }
   };
 
-  getCounterfactualCovidCases = async (
-    iso_code,
-    start_date,
-    end_date,
-    first_restriction,
-    lockdown_date
-  ) => {
-    const data = await this.#getDailyCounterfactualCovidCases(
-      "counterfactual",
-      iso_code,
-      start_date,
-      end_date,
-      first_restriction,
-      lockdown_date
-    );
-    return data;
-  };
-
   getRealCovidCases = async (iso_code, start_date, end_date) => {
-    const data = await this.#getDailyCovidCases(
-      "real",
-      iso_code,
-      start_date,
-      end_date
-    );
-    return data;
+    try {
+      const target = `http://localhost:8000/api/cases/real/daily/normalised/?iso_code=${iso_code}&start_date=${start_date}&end_date=${end_date}`;
+      console.debug(`Backend ${target}`);
+      const response = await axios.get(target, {});
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
   };
 }
 
