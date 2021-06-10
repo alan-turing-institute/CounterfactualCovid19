1fbcc60dacdf0e7a94fe8c7051301b924cf5a7a6
James Robinson <james.em.robinson@gmail.com>
2021-06-10 11:27:33 +0200

    :bug: Only apply axios-retry to the geometry retrieval

--- frontend/src/tasks/LoadCountryGeometryTask.js
+++ frontend/src/tasks/LoadCountryGeometryTask.js
@@ -4,16 +4,18 @@ import axiosRetry from "axios-retry";
 // Asynchronously load country demographic data from Django backend
 const loadGeometriesTask = async () => {
   try {
-    const target = "http://localhost:8000/api/country/geometry/";
-    console.debug(`Backend ${target}`);
-    axiosRetry(axios, {
+    const baseUrl = "http://localhost:8000";
+    const endpoint = "/api/country/geometry/";
+    console.debug(`Backend ${baseUrl}${endpoint}`);
+    const request = axios.create({ baseURL: baseUrl });
+    axiosRetry(request, {
       retries: 10, // number of retries
       retryDelay: (retryCount) => {
         console.log(`Backend retry attempt: ${retryCount}/10`);
         return retryCount * 2000; // time interval between retries
       },
     });
-    const response = await axios.get(target, {});
+    const response = await request.get(endpoint, {});
     return response.data.features;
   } catch (error) {
     console.log(error);
